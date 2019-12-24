8adc5ccac01f0c30ab388fbfdd9d6b5f88231f71
Junya Oiwa <0920oiwa@gmail.com>
2019-12-24 23:41:34 +0900

    OGP設定を追記

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,17 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Tokyo City University Advent Calendar 2019 特設サイト',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { property: 'og:url', content: 'https://tcuac2019.netlify.com/'},
+      { property: 'og:title', content: 'Tokyo City University Advent Calendar 2019 特設サイト'},
+      { property: 'og:site_name', content: 'Tokyo City University Advent Calendar 2019 特設サイト'},
+      { property: 'og:image', content: 'https://tcuac2019.netlify.com/img/ogp.png'},
+      { property: 'og:description', content: '2月のはじめからクリスマスまでの25日間、東京都市大学に在籍する学生が持ち回りで自分の好きな話題について記事を執筆するイベントです。'},
+      { property: 'twitter:card', content: 'summary_large_image'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
